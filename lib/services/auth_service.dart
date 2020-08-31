a9351bf7ca5325aacea29dc4ef4499dc13572f18
Mohammed Moin Ghadiyali <mdmoin786313@gmail.com>
2020-08-31 15:17:04 +0530

    Firebase Changes

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,9 +1,9 @@
-import 'package:ImageTagging/screens/splashscreen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ImageTagging/models/user_data.dart';
 import 'package:provider/provider.dart';
+import 'package:ImageTagging/screens/LoginScreen.dart';
 
 class AuthService {
   static final _auth = FirebaseAuth.instance;
